388011b4d98779b1095571f28c7d769510dc2c8f
Luis López <luislm1986@gmail.com>
2025-03-24 12:31:05 +0100

    fury ok 2

--- todo.js
+++ todo.js
@@ -1,4 +1,56 @@
 
+let clinicas = [
+  {
+    nombreCentro: "Huellas Felices",
+    servicios: ["veterinario", "peluqueria canina"],
+    localidad: "Madrid",
+  },
+  {
+    nombreCentro: "Amigos Peludos",
+    servicios: ["hotel", "cuidador", "veterinario"],
+    localidad: "Barcelona",
+  },
+  {
+    nombreCentro: "Cola y Bigote",
+    servicios: ["peluqueria canina", "cuidador"],
+    localidad: "Valencia",
+  },
+  {
+    nombreCentro: "Patitas Sanas",
+    servicios: ["veterinario"],
+    localidad: "Sevilla",
+  },
+  {
+    nombreCentro: "Mimos Vet",
+    servicios: ["hotel", "cuidador"],
+    localidad: "Bilbao",
+  },
+  {
+    nombreCentro: "Cuatro Patas",
+    servicios: ["veterinario", "cuidador", "peluqueria canina"],
+    localidad: "Zaragoza",
+  },
+  {
+    nombreCentro: "Pelo Suelto",
+    servicios: ["peluqueria canina", "hotel"],
+    localidad: "Málaga",
+  },
+  {
+    nombreCentro: "La Caseta",
+    servicios: ["hotel"],
+    localidad: "A Coruña",
+  },
+  {
+    nombreCentro: "Vet&Love",
+    servicios: ["veterinario", "cuidador"],
+    localidad: "Granada",
+  },
+  {
+    nombreCentro: "Patas Urbanas",
+    servicios: ["cuidador", "peluqueria canina"],
+    localidad: "Madrid",
+  }
+];
 
 const links = [
   { 
@@ -64,8 +116,6 @@ function renderClinicas(localidadSeleccionada = "todas", servicioSeleccionado = 
 
     const localidadClinica = document.createElement("p");
     localidadClinica.textContent = `Localidad: ${clinica.localidad}`;
-
-    //BOTON CITA
 
     tarjetaClinica.append(nombreClinica, serviciosClinica, localidadClinica);
     clinicaList.appendChild(tarjetaClinica);

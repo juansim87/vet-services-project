e7ed0d684139a11803bb61fbd3eca1a23ea3a8e8
Usuario <juansim87@gmail.com>
2025-03-24 13:56:10 +0100

    reordering components

--- index.js
+++ index.js
@@ -1,53 +1,53 @@
-let clientes = [
-  {
-    id: 1,
-    isEditing: false,
-    mascota: "Atapuerco",
-    especie: "cerdo",
-    raza: "vietnamita",
-    imagen: "/media/client-pets/atapuerco.webp",
-    isShowing: false,
-    cliente: "Juan Simón",
-    email: "juan.simon@pet-it.com",
-    proximaCita: "2025-03-06",
-  },
-  {
-    id: 2,
-    isEditing: false,
-    mascota: "Piticli",
-    especie: "loro",
-    raza: "",
-    imagen: "/media/client-pets/piticli.webp",
-    isShowing: false,
-    cliente: "Luis Fury",
-    email: "fury.lopez@pet-it.com",
-    proximaCita: "2025-03-06",
-  },
-  {
-    id: 3,
-    isEditing: false,
-    mascota: "Freya",
-    especie: "perro",
-    raza: "",
-    imagen: "/media/client-pets/freya.webp",
-    isShowing: false,
-    cliente: "Luis Simón",
-    email: "luis.simon@pet-it.com",
-    proximaCita: "2025-03-06",
-  },
-  {
-    id: 4,
-    isEditing: false,
-    mascota: "Brunelleschi",
-    especie: "gato",
-    raza: "",
-    imagen: "/media/client-pets/brunelleschi.webp",
-    isShowing: false,
-    cliente: "Eugenio Drosdov",
-    email: "far.drosdov@pet-it.com",
-    proximaCita: "2025-03-06",
-  },
-];
+// let clientes = [
+//   {
+//     id: 1,
+//     isEditing: false,
+//     mascota: "Atapuerco",
+//     especie: "cerdo",
+//     raza: "vietnamita",
+//     imagen: "/media/client-pets/atapuerco.webp",
+//     isShowing: false,
+//     cliente: "Juan Simón",
+//     email: "juan.simon@pet-it.com",
+//     proximaCita: "2025-03-06",
+//   },
+//   {
+//     id: 2,
+//     isEditing: false,
+//     mascota: "Piticli",
+//     especie: "loro",
+//     raza: "",
+//     imagen: "/media/client-pets/piticli.webp",
+//     isShowing: false,
+//     cliente: "Luis Fury",
+//     email: "fury.lopez@pet-it.com",
+//     proximaCita: "2025-03-06",
+//   },
+//   {
+//     id: 3,
+//     isEditing: false,
+//     mascota: "Freya",
+//     especie: "perro",
+//     raza: "",
+//     imagen: "/media/client-pets/freya.webp",
+//     isShowing: false,
+//     cliente: "Luis Simón",
+//     email: "luis.simon@pet-it.com",
+//     proximaCita: "2025-03-06",
+//   },
+//   {
+//     id: 4,
+//     isEditing: false,
+//     mascota: "Brunelleschi",
+//     especie: "gato",
+//     raza: "",
+//     imagen: "/media/client-pets/brunelleschi.webp",
+//     isShowing: false,
+//     cliente: "Eugenio Drosdov",
+//     email: "far.drosdov@pet-it.com",
+//     proximaCita: "2025-03-06",
+//   },
+// ];
 
 //NAV
 
@@ -176,6 +176,10 @@ clientEmailInput.placeholder = "Ej: juan.simon@mascotero.com";
 
 clientEmail.append(clientEmailLabel, clientEmailInput);
 
+
+
+
+
 //BOTÓN SUBMIT
 
 const submitCreateNewClient = (event) => {
@@ -204,12 +208,6 @@ const submitCreateNewClient = (event) => {
   renderClients();
 };
 
-const deleteClient = (clientId) => {
-  console.log("cliente", clientId);
-  // clientes = clientes.filter(client => client.id !== clientId);
-  renderClients();
-}
-
 const btnSubmitRegister = document.createElement("button");
 btnSubmitRegister.type = "submit";
 btnSubmitRegister.textContent = "Registrarse";
@@ -232,155 +230,12 @@ clientFormSection.appendChild(clientFormBox);
 
 console.log(clientFormBox);
 
-const renderClients = () => {
-  const container = document.getElementById("client-database");
-  container.innerHTML = "";
-  clientes.forEach((client) => {
-    const clientCard = document.createElement("div");
-    clientCard.classList.add("client-card");
-
-    const petName = document.createElement("h2");
-    petName.textContent = `${client.mascota}`;
-
-    const petImg = document.createElement("img");
-    petImg.setAttribute("src", client.imagen);
-    petImg.classList.add("hidden");
-
-    /**
-     * Problemas con la lógica de este botón. Al final optamos por toggle para facilitarla.
-     */
-
-    const btnShowImg = document.createElement("button");
-    btnShowImg.type = "button";
-    btnShowImg.textContent = "Mostrar Imagen";
-
-    btnShowImg.addEventListener("click", () => {
-      client.isShowing = !client.isShowing;
-
-      petImg.classList.toggle("hidden");
-
-      if (!client.isShowing) {
-        btnShowImg.textContent = "Mostrar Imagen";
-      } else {
-        btnShowImg.textContent = "Ocultar Imagen";
-      }
-    });
-
-    const petSpecies = document.createElement("p");
-    petSpecies.textContent = `Especie: ${client.especie}`;
-
-    const petBreed = document.createElement("p");
-    if (client.raza !== "") {
-      petBreed.textContent = `Raza: ${client.raza}`;
-    } else {
-      petBreed.classList.add("hidden");
-    }
-
-    const nextDate = document.createElement("p");
-    nextDate.textContent = `Próxima cita: ${client.proximaCita}`;
-
-    const clientName = document.createElement("p");
-    clientName.textContent = `Cliente: ${client.cliente}`;
-
-    const clientEmail = document.createElement("p");
-    clientEmail.textContent = `Email: ${client.email}`;
-
-    const btnEdit = document.createElement("button");
-    btnEdit.textContent = "Editar cliente";
-    btnEdit.addEventListener("click", () => {
-      client.isEditing = !client.isEditing;
-      renderClients();
-    });
-
-    const btnDelete = document.createElement("button");
-    btnDelete.textContent = "Eliminar ficha";
-    btnDelete.addEventListener("click", deleteClient);
-
-    clientCard.append(
-      petName,
-      btnShowImg,
-      petImg,
-      petSpecies,
-      petBreed,
-      clientName,
-      clientEmail,
-      nextDate,
-      btnEdit,
-      btnDelete
-    );
-
-    if (client.isEditing) {
-      const form = document.createElement("form");
-
-      const clientNameBox = document.createElement("div");
-
-      const clientNameLabel = document.createElement("label");
-      clientNameLabel.textContent = "Corregir nombre de cliente";
-
-      const clientName = document.createElement("input");
-      clientName.id = `edit-cliente-${client.id}`;
-      clientName.value = client.cliente;
-      
-      clientNameBox.append(clientNameLabel, clientName);
 
 
-      const speciesBox = document.createElement("div");
+/**
+ * 
+ */
 
-      const petSpeciesLabel = document.createElement("label");
-      petSpeciesLabel.textContent = "Corregir especie:";
-
-      const petSpecies = document.createElement("input");
-      petSpecies.id = `edit-species-${client.id}`;
-      petSpecies.value = client.especie;
-
-      speciesBox.append(petSpeciesLabel, petSpecies);
-
-      const breedBox = document.createElement("div");
-      const petBreedLabel = document.createElement("label");
-      petBreedLabel.textContent = "Corregir raza:";
-
-      const petBreed = document.createElement("input");
-      petBreed.id = `edit-breed-${client.id}`;
-      petBreed.value = client.raza;
-
-      breedBox.append(petBreedLabel, petBreed);
-
-      const dateBox = document.createElement("div");
-
-      const nextDateLabel = document.createElement("label");
-      nextDateLabel.textContent = "Modificar próxima cita: ";
-      
-      const nextDate = document.createElement("input");
-      nextDate.type = "date";
-      nextDate.id = `edit-date-${client.id}`;
-      nextDate.value = client.proximaCita;
-
-      dateBox.append(nextDateLabel, nextDate)
-
-      const btnSubmitEdit = document.createElement("button");
-      btnSubmitEdit.type = "submit";
-      btnSubmitEdit.textContent = "Editar";
-      
-      form.addEventListener("submit", (event) => {
-        event.preventDefault();
-        console.log("submit form");
-
-        editClient(client.id, {
-          cliente: clientName.value,
-          especie: petSpecies.value,
-          proximaCita: nextDate.value,
-        });
-        event.target.reset();
-      });
-
-      form.append(clientNameBox, speciesBox, breedBox, dateBox, btnSubmitEdit);
-
-      clientCard.appendChild(form);
-    }
-
-    container.append(clientCard);
-  });
-};
 
 renderClients();
 
@@ -394,5 +249,3 @@ const editClient = (id, fields) => {
   }
   renderClients();
 };
-
-

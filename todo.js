3082330d6215d0006472fc464e9d03f813cc69fb
Luis López <luislm1986@gmail.com>
2025-03-24 12:29:59 +0100

    fury ok

--- todo.js
+++ todo.js
@@ -1,86 +1,4 @@
-let clientes = [
-  {
-    mascota: "Atapuerco",
-    especie: "cerdo",
-    imagen: "url()",
-    cliente: "Juan Simón",
-    "próxima cita": "",
-  },
-  {
-    mascota: "Piticli",
-    especie: "loro",
-    imagen: "url()",
-    cliente: "Luis Fury",
-    "próxima cita": "",
-  },
-  {
-    mascota: "Freya",
-    especie: "perro",
-    imagen: "url()",
-    cliente: "Luis Simón",
-    "próxima cita": "",
-  },
-  {
-    mascota: "Brunelleschi",
-    especie: "gato",
-    imagen: "url()",
-    cliente: "Eugenio Drosdov",
-    "próxima cita": "",
-  },
-];
 
-let clinicas = [
-  {
-    nombreCentro: "Huellas Felices",
-    servicios: ["veterinario", "peluqueria canina"],
-    localidad: "Madrid",
-  },
-  {
-    nombreCentro: "Amigos Peludos",
-    servicios: ["hotel", "cuidador", "veterinario"],
-    localidad: "Barcelona",
-  },
-  {
-    nombreCentro: "Cola y Bigote",
-    servicios: ["peluqueria canina", "cuidador"],
-    localidad: "Valencia",
-  },
-  {
-    nombreCentro: "Patitas Sanas",
-    servicios: ["veterinario"],
-    localidad: "Sevilla",
-  },
-  {
-    nombreCentro: "Mimos Vet",
-    servicios: ["hotel", "cuidador"],
-    localidad: "Bilbao",
-  },
-  {
-    nombreCentro: "Cuatro Patas",
-    servicios: ["veterinario", "cuidador", "peluqueria canina"],
-    localidad: "Zaragoza",
-  },
-  {
-    nombreCentro: "Pelo Suelto",
-    servicios: ["peluqueria canina", "hotel"],
-    localidad: "Málaga",
-  },
-  {
-    nombreCentro: "La Caseta",
-    servicios: ["hotel"],
-    localidad: "A Coruña",
-  },
-  {
-    nombreCentro: "Vet&Love",
-    servicios: ["veterinario", "cuidador"],
-    localidad: "Granada",
-  },
-  {
-    nombreCentro: "Patas Urbanas",
-    servicios: ["cuidador", "peluqueria canina"],
-    localidad: "Madrid",
-  }
-];
 
 const links = [
   { 
@@ -146,6 +64,8 @@ function renderClinicas(localidadSeleccionada = "todas", servicioSeleccionado = 
 
     const localidadClinica = document.createElement("p");
     localidadClinica.textContent = `Localidad: ${clinica.localidad}`;
+
+    //BOTON CITA
 
     tarjetaClinica.append(nombreClinica, serviciosClinica, localidadClinica);
     clinicaList.appendChild(tarjetaClinica);
